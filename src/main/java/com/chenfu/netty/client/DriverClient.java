290dff608676c3ff76a1325993fb3e3950afa23d
chenfu <211147056@qq.com>
2018-10-08 17:54:49 +0800

    完成json to pojo,opjo to json 转换

--- src/main/java/com/chenfu/netty/client/DriverClient.java
+++ src/main/java/com/chenfu/netty/client/DriverClient.java
@@ -1,9 +1,6 @@
 package com.chenfu.netty.client;
 
-import com.chenfu.pojo.Coordinate;
-import com.chenfu.pojo.DataContent;
-import com.chenfu.pojo.Driver;
-import com.chenfu.pojo.MsgActionEnum;
+import com.chenfu.pojo.*;
 import com.chenfu.utils.JsonUtils;
 
 import java.io.DataOutputStream;
@@ -11,24 +8,24 @@ import java.io.InputStream;
 import java.net.Socket;
 
 public class DriverClient {
-    public static void main(String[] args) {
-        try{
-            Socket soc = new Socket("127.0.0.1",8888);
-            DataOutputStream dos = new DataOutputStream(soc.getOutputStream());
-            DataContent dataContent = new DataContent();
-            dataContent.setAction(MsgActionEnum.DRIVER_COORDIANATE.type);
-            Driver driver = new Driver("asd",123.23,23.111,4,5,6);
-            dataContent.setObject(driver);
-            String json = JsonUtils.objectToJson(dataContent);
-            System.out.println(json);
-            dos.writeUTF(json);
-            byte[] bytes = new byte[100];
-            InputStream dataIutputStream = soc.getInputStream();
-            dataIutputStream.read(bytes);
-            String s = new String(bytes, "UTF-8");
-            System.out.println(s);
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-    }
+//    public static void main(String[] args) {
+//        try{
+//            Socket soc = new Socket("127.0.0.1",8888);
+//            DataOutputStream dos = new DataOutputStream(soc.getOutputStream());
+//            DataContent dataContent = new DataContent();
+//            dataContent.setAction(MsgActionEnum.DRIVER_COORDIANATE.type);
+//            Driver driver = new Driver("asd",123.23,23.111,4,5,6);
+//            dataContent.setObject(driver);
+//            String json = JsonUtils.objectToJson(dataContent);
+//            System.out.println(json);
+//            dos.writeUTF(json);
+//            byte[] bytes = new byte[100];
+//            InputStream dataIutputStream = soc.getInputStream();
+//            dataIutputStream.read(bytes);
+//            String s = new String(bytes, "UTF-8");
+//            System.out.println(s);
+//        }catch (Exception e){
+//            e.printStackTrace();
+//        }
+//    }
 }
